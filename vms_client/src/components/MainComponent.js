ace6882411e925d70e39244876b268bfcf15ae3d
Shristhi Pahwa <shristhi.pahwa24@gmail.com>
2020-10-27 12:24:29 +0530

    added css

--- vms_client/src/components/MainComponent.js
+++ vms_client/src/components/MainComponent.js
@@ -4,8 +4,9 @@ import { USERS } from "../shared/users";
 import UserDetails from "./UserdetailsComponent";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Footer from "./FooterComponent";
-
-import Mycomponent from "./SidebarComponent";
+import Header from "./HeaderComponent";
+import SideNavv from "./SidebarComponent";
+import { baseUrl } from "../shared/baseUrl";
 
 class Main extends Component {
   constructor(props) {
@@ -14,16 +15,19 @@ class Main extends Component {
       users: USERS,
     };
   }
-
+  
+ // componentDidMount() {
+    //fetch(baseUrl + "residents")
+      //.then((response) => response.json())
+     // .then((residents) => this.setState({ users:residents }));
+ // }
   render() {
     return (
-      <div>
-        <div className="row ">
-          <div className="col-12 col-md-2 sidebar full">
-            <Mycomponent />
-          </div>
+      <div class="full">
+        <Header />
+        <div className="row row-margin">
           <Switch>
-            <div className="col-md-9  full">
+            <div className="col-md">
               <Route path="/register" component={Registeration} />
               <Route
                 exact
@@ -31,7 +35,7 @@ class Main extends Component {
                 component={() => (
                   <UserDetails
                     user={
-                      this.state.users.filter(
+                       this.state.users.filter(
                         (user) => user.Registerednum === "UP15AZ3047"
                       )[0]
                     }
